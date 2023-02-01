25b6236610ae6d03468ebf2229aa339bc202967e
Timi-Artturi Mäkelä <timi.makela@geniem.com>
2023-02-01 17:42:32 +0200

    swedish translations

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Cookiebot Helper
  * Plugin URI: https://github.com/devgeniem/wp-cookiebot-helper
  * Description: Helper plugin for handling Cookiebot consent states
- * Version: 1.1.3
+ * Version: 1.1.4
  * Requires PHP: 7.0
  * Author: Geniem Oy / Hermanni Piirainen
  * Author URI: https://geniem.com
